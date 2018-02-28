014927b5d2fab2d6c77e3282b38d4f027f6b1a83
Anton <anton11061998@gmail.com>
2018-03-01 01:24:46 +0300

    Create Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+ var rez = 0;
+  do{
+  	number = Math.floor(number / 5);
+  	rez=rez+ number;
+  }
+
+  while(number >= 5 );
+  return rez;
 }
+
+
